a6762dfe2331bd8b6d19ff13d6a9a35912a97c5d
alex h <alexander.hallpnw2@icloud.com>
2023-10-19 00:08:07 -0700

    added social links

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,6 +1,8 @@
 import Header from "./Header";
 import Footer from "./Footer";
 
+import Instagram from "../assets/Images/contact/icons8-instagram-250.png";
+
 import "../assets/Css/Contact.css";
 
 export default function Contact() {
@@ -9,7 +11,21 @@ export default function Contact() {
       <Header />
       <div className="contactSectionOne">
         <h1>CONTACT US</h1>
+
+        <h3>Email</h3>
         <a href="mailto:tom@nwintuitivefit.com">tom@nwintuitivefit.com</a>
+      </div>
+      <div className="contactSectionTwo">
+        <h3>Social Links</h3>
+        <div className="socialLinks">
+          <a
+            href="https://www.instagram.com/intuitive.fitness.seattle"
+            target="_blank"
+          >
+            {" "}
+            <img src={Instagram} alt="instagram icon" />
+          </a>
+        </div>
       </div>
       <Footer />
     </div>
