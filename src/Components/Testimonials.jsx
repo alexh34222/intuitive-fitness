4a92787cbb398a48568afbd024b5028e9e5fbead
alex h <alexander.hallpnw2@icloud.com>
2023-10-24 16:55:47 -0700

    started adding reviews

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -7,24 +7,110 @@ export default function Testimonials() {
     <div>
       <Header />
       <div className="clientTestimonialsPage">
-        <div className="testimonialsMainText">
-          <p>
-            From the early stages of life, we naturally engage in activities
-            like crawling, walking, and running, as well as playful movements.
-            Throughout history, our ancestors relied on physical activity for
-            survival, such as hunting, gathering, and farming. <br /> <br />
-            In modern times, exercise remains instinctive as it releases
-            endorphins, the feel-good hormones, contributing to improved mood
-            and reduced stress. Whether it's dancing, playing sports, or simply
-            taking a walk, our bodies are designed to be in motion. <br />{" "}
-            <br />
-            The beauty of exercise is that it allows us to listen to our bodies,
-            choosing activities that bring joy and fulfillment, making it more
-            likely to become a sustainable and enjoyable part of our lives.{" "}
-            <br />
-            <br />
-            Here are some clients who make Intuitive the empowering place it is.
-          </p>
+        <div className="testimonialsSectionOne">
+          <h2>Client Testimonials</h2>
+          <div className="testimonialsMainText">
+            <p>
+              From the early stages of life, we naturally engage in activities
+              like crawling, walking, and running, as well as playful movements.
+              Throughout history, our ancestors relied on physical activity for
+              survival, such as hunting, gathering, and farming. <br /> <br />
+              In modern times, exercise remains instinctive as it releases
+              endorphins, the feel-good hormones, contributing to improved mood
+              and reduced stress. Whether it's dancing, playing sports, or
+              simply taking a walk, our bodies are designed to be in motion.{" "}
+              <br /> <br />
+              The beauty of exercise is that it allows us to listen to our
+              bodies, choosing activities that bring joy and fulfillment, making
+              it more likely to become a sustainable and enjoyable part of our
+              lives. <br />
+              <br />
+              Here are some clients who make Intuitive the empowering place it
+              is.
+            </p>
+          </div>
+        </div>
+        <div className="testimonialsSectionTwo">
+          <div className="testimonial">
+            {" "}
+            <h3>Abhinav T. - Jun 22, 2020</h3>
+            <p>
+              Tom is awesome! He has been my personal trainer for over a year
+              now and he has been amazing. I had a pretty serious back injury a
+              couple of years ago and he has tailored my workout to both help me
+              strengthen my core but also generally improve my health. He’s a
+              really chill guy that is understanding of everything that is
+              happening and I can not recommend him more!
+            </p>
+          </div>
+          <div className="testimonial">
+            <h3>Tarik H. - Apr 13, 2021 · Hired on Thumbtack</h3>
+            <p>
+              Tom has been the reason my husband and I finally enjoy working
+              out, and we never used to work out. I would highly recommend him
+              to anyone looking for a well informed, easy-going, no-guilt
+              trainer. Not to mention the privacy of his well set up backyard
+              workout shed is a pleasure to workout in as opposed to a crowded
+              gym!
+            </p>
+          </div>
+          <div className="testimonial">
+            <h3>Britton Q. - May 5, 2021 · Hired on Thumbtack</h3>
+            <p>
+              Tom is knowledgeable, friendly, and professional. He's giving me
+              new training methods and form improvements to improve my squat,
+              deadlift and bench press.
+            </p>
+          </div>
+          <div className="testimonial">
+            <h3>Jazzy T. - Jan 20, 2022 · Hired on Thumbtack</h3>
+            <p>
+              Tom is an amazing trainer - very knowledgeable and understanding.
+              Getting back to shape is hard but he knows how to make it
+              engaging. He knows exactly how to push you to get the best
+              results.
+            </p>
+          </div>
+          <div className="testimonial">
+            <h3></h3>
+            <p></p>
+          </div>
+          <div className="testimonial">
+            <h3></h3>
+            <p></p>
+          </div>
+          <div className="testimonial">
+            <h3></h3>
+            <p></p>
+          </div>
+          <div className="testimonial">
+            <h3></h3>
+            <p></p>
+          </div>
+          <div className="testimonial">
+            <h3></h3>
+            <p></p>
+          </div>
+          <div className="testimonial">
+            <h3></h3>
+            <p></p>
+          </div>
+          <div className="testimonial">
+            <h3></h3>
+            <p></p>
+          </div>
+          <div className="testimonial">
+            <h3></h3>
+            <p></p>
+          </div>
+          <div className="testimonial">
+            <h3></h3>
+            <p></p>
+          </div>
+          <div className="testimonial">
+            <h3></h3>
+            <p></p>
+          </div>
         </div>
       </div>
 
